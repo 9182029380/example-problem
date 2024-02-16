712d92a24bee7b08345f78a58f8e1606ba365fe0
9182092380 <pothulasharath452@gmail.com>
2024-02-16 21:29:58 +0530

    changesdone

--- src/main/java/com/example/EventManagement/controller/EventController.java
+++ src/main/java/com/example/EventManagement/controller/EventController.java
@@ -1,2 +1,14 @@
-package com.example.EventManagement.controller;public class EventController {
+package com.example.EventManagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+    @GetMapping("/event")
+    public String hello(){
+
+        return "hi all welcome to the my events ";
+    }
+
 }
